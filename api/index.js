e3a6c730b038784e59de1adf6b102cd2e678d8d1
quangle0611 <67314577+quangle0611@users.noreply.github.com>
2020-08-13 14:47:30 +0700

    bắt đầu khởi tạo file api

--- api/index.js
+++ api/index.js
@@ -1,15 +1,25 @@
-var path = require('path');
-var express = require('express');
-var serveStatic = require('serve-static');
+var express = require("express");
 var app = express();
+app.set("view engine", "ejs");
+app.set("views", "./views");
+app.use(express.static("public"));
+app.listen(3000);
 
-app.use(serveStatic(path.join(__dirname, 'public')));
+//body-parser admin-quang123
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//mongoose
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://admin:quang123@cluster0.ss48k.mongodb.net/QAlbum?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true, function(err) {
+    if(err) {
+        console.log("Mongodb connected error!");
+    } else{
+        console.log("Mongodb connected successfully!");
+    }
+}});
 
 
-app.listen(3000,function(){
-    console.log('Node-server running...')
-});
-
-app.get('/', function(request, response){
-  response.redirect('../client/src/index.html');
+app.get("/", function(req, res){
+    res.render("home");
 });
